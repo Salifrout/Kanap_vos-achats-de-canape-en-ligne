adabb70dcde5f7aa5e277312092c3a3f3293cc69
Salifrout <86348670+Salifrout@users.noreply.github.com>
2021-10-31 17:51:48 +0100

    DOM script.js 8

--- back/app.js
+++ back/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const productRoutes = require('./routes/product');
 
 const app = express();
+const cors = require('cors');
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -11,6 +12,10 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+
+app.use(cors({
+  origin: /^http\:\/\/localhost:[\d]{4}$/
+}));
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.static('images'));
