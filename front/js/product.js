8a4dda999a618b5dee0af31792b3622eaa88345d
Salifrout <86348670+Salifrout@users.noreply.github.com>
2021-11-14 12:35:23 +0100

    modification fetch

--- front/js/product.js
+++ front/js/product.js
@@ -5,9 +5,14 @@ let url = new URL(URLofpage);
 //créer une variable pour définir la valeur du paramètre id situé dans l'URL
 let product_ID = url.searchParams.get("id");
 
+var value_id = '';
+let value_altTxt = '';
+let value_imageUrl = '';
+let value_price = '';
+let value_name = '';
+let value_description = ''; 
 
-
-
+let blob = "http://localhost:3000/api/products/" + product_ID;
 const OnerequestState = fetch("http://localhost:3000/api/products/" + product_ID);
 
 //parse in pour vérifier que ce soit un entier,  /product_id et réupère un seul objet produitt
@@ -15,28 +20,33 @@ const OnerequestState = fetch("http://localhost:3000/api/products/" + product_ID
 //envisager qu'il n'y ai pas de ID dans l'URL de la page et écrire une phrase pour dire à l'utilisateur qu'il faut remplir son panier
 
 //requêter l'API et faire apparaitre le produit sélectionné depuis la page d'accueil
-OnerequestState
+function GetInfosforOneProduct() {OnerequestState
     .then(function(res) {
         console.info(res);
         return res.json();
     })
     .then(function(value) {
-        //if (product_ID == null || product_ID == undefined) {
-     
-          //  document.getElementsByTagName("article").innerHTML ="Une erreur est survenue. Vous serez redirigé vers la page d'accueil dans 5 secondes."
-          //  setTimeout(document.location.href = "https://salifrout.github.io/Projet-Kanap/front/html/index.html", 5000);
-        
-        //} else { if  //(product_ID !== undefined) {
-            console.log(value);
+        if (!product_ID) {
+            
+            alert("Une erreur est survenue. Vous allez être redirigé vers la page d'accueil dans quelques instants.");
+            //let phrase_err = document.createElement("p");
+            //document.getElementsByTagName("section").replaceChild(document.getElementsByTagName("article"), phrase_err);
+            //phrase_err.innerHTML = "Une erreur est survenue. Vous serez redirigé vers la page d'accueil dans 5 secondes.";
+            setTimeout(document.location.href = "https://salifrout.github.io/Projet-Kanap/front/html/index.html", 5000);
+            return false;
+
+        } else if (product_ID !== undefined) {
         
             let product_img = document.createElement("img");
-            document.getElementsByClassName('item__img').appendChild(product_img);
-            product_img.setAttribute("alt", value.altTxt + ", " + value.name);
-            product_img.innerHTML = value.imageUrl;
+            document.querySelector("section.item article div.item__img").appendChild(product_img);
+            product_img.setAttribute("alt", value_altTxt + ", " + value_name);
+            product_img.setAttribute("src", value_imageUrl);
 
-            document.getElementById("title").innerHTML = value.name;
-            document.getElementById("price").innerHTML = value.price;
-            document.getElementById("description").innerHTML = value.description;
+            document.getElementById("title").innerHTML = value_name;
+            document.getElementById("price").innerHTML = value_price / 100;
+            document.getElementById("description").innerHTML = value_description;
+
+            document.getElementById("colors").options.length = 0;
 
             let colorsofproduct = value.colors;
             for (let color of colorsofproduct) {
@@ -45,15 +55,37 @@ OnerequestState
                 colorInOption.setAttribute("option", color);
                 colorInOption.innerHTML = color;
             }
-       // }
+
+            //info_id = value._id;
+            //info_altTxt = value.altTxt;
+            //info_imageUrl = value.imageUrl;
+            //info_price = value.price;
+            //info_name = value.name;
+
+            let Infos_OneProduct = [value_id, value_imageUrl, info_altTxt, info_name, info_price];
+            return Infos_OneProduct;
+
+        } else {
+            //essayer display none sur l'article ou la section puis set time out renvoyer vers page d'accueil
+        }
     })
     .catch(function(err) {
         //prévenir en cas d'erreur
-        console.info(err);
-        console.alert(err);
         console.log("Une nouvelle erreur empêche le résultat de s'afficher.")
-    })
-;
+    });
+}
+
+//var value_id = blob.res._id;
+//let value_altTxt = value.altTxt;
+//let value_imageUrl = value.imageUrl;
+//let value_price = blob.res.price;
+//let value_name = value.name;
+//let value_description = value.description;
+
+let Array1 = GetInfosforOneProduct;
+
+console.log("un problème est survenu: " + Array1[0]);
+
 
 //créer une classe pour mettre des informations sur chaque produit
 class Product {
@@ -71,13 +103,18 @@ class Product {
 let OneProduct = {};
 
 //les fonctions suivantes permettent de créer une nouvelle classe, mettre dans un tableau, vider le localstorage avant d'y remettre le tableau mis à jour
-function CreateProductForCart(Chosenproduct) {
+function CreateProductForCart() {
+    if (document.getElementById("quantity").value == 0 || document.getElementById("quantity").value > 100) {
+        alert("La quantité choisie pour votre produit n'est pas possible. Veuillez choisir une quantité différente.");
+        return false;
+    } else {
     let quantite = document.getElementById("quantity").value;
     let couleur = document.getElementById("colors").value;
 
-    let OneProduct = new Product(Chosenproduct.value._id, quantite, couleur, Chosenproduct.value.imageUrl, Chosenproduct.value.altTxt, Chosenproduct.value.name, Chosenproduct.value.price);   
+    let OneProduct = new Product(OnerequestState.then(function(value) {value.id}), quantite, couleur, OnerequestState.then(function(value) {value.imageUrl}), OnerequestState.then(function(value) {value.altTxt}), OnerequestState.then(function(value) {value.name}), OnerequestState.then(function(value) {value.price}));   
         
-    console.log("la nouvelle instance peut être créée.")    
+    return true;
+    }    
 }
 
 let Cart = [];
@@ -109,14 +146,31 @@ function UpdateStorage() {
 
 function AddNewProductInStorage() {
     if (product_ID !== undefined) {
-        CreateProductForCart(OnerequestState);
-        UpdateCart(OneProduct);
-        UpdateStorage;
-
-        console.log("Le panier est mis à jour.") 
+        CreateProductForCart();
+        if (CreateProductForCart()) {
+            console.log("la nouvelle instance peut être créée.");
+            UpdateCart(OneProduct);
+            UpdateStorage;
+            console.log("Le panier est mis à jour."); 
+            return true;
+        } else {
+            return false;
+        }
     } else {
     }
 }
 
 //ajouter dans le panier lors du click sur le bouton
 document.getElementById("addToCart").addEventListener('click', AddNewProductInStorage);
+
+
+
+
+
+
+
+
+
+
+
+
